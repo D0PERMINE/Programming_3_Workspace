ea7dcbe8cc4e6ad1fac122a2f9ccd42555208ab3
Timo Ji <timo.ji@student.htw-berlin.de>
2022-04-22 08:44:02 +0200

    Uebung 03 has been updated

---
+++ Uebung_03/src/SparbuchTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import bankprojekt.verarbeitung.Kunde;
+import bankprojekt.verarbeitung.Sparbuch;
+//import bankprojekt.verarbeitung.Waehrung;
+
+
+class SparbuchTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+//		Sparbuch(Kunde inhaber, long kontonummer, Waehrung kontoWaehrung)
+		Sparbuch sparbuch1 = new Sparbuch();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
