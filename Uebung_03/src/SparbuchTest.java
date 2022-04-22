1de87361ffd67bad7b267457a59883159e0afa9e
Timo Ji <timo.ji@student.htw-berlin.de>
2022-04-22 12:25:12 +0200

    Uebung 03 has been updated and abgab03_improve was added

--- Uebung_03/src/SparbuchTest.java
+++ Uebung_03/src/SparbuchTest.java
@@ -12,24 +12,35 @@ import bankprojekt.verarbeitung.Sparbuch;
 
 
 class SparbuchTest {
-
+	/*
+	 * 1) Alle Methoden mal aufrufen
+	 * 2) Rueckgabewert 
+	 * 		+ Auswirkungen pruefen
+	 * 3) mehrere Methodenaufrufe kombinieren
+	 */
+	
+	Kunde kunde1 = new Kunde();
+	Sparbuch sparbuch1 = new Sparbuch();
 	@BeforeEach
 	void setUp() throws Exception {
 //		Sparbuch(Kunde inhaber, long kontonummer)
 //		Kunde(String vorname, String nachname, String adresse, LocalDate gebdat)
-//		System.out.println("Anlagen von Kunden: ");
-//		Kunde kunde2 = new Kunde();
-//		Kunde kunde3 = new Kunde();
-//		System.out.println("Anlagen von Sparbeuchern: ");
-//		Sparbuch sparbuch2 = new Sparbuch();
-//		Sparbuch sparbuch3 = new Sparbuch();
+		kunde1 = new Kunde("Timo", "Ji", "Storkower Strasse 219", LocalDate.of(1996, 07, 31));
+		sparbuch1 = new Sparbuch(kunde1, 12345678);
 	}
 
 	@Test
-	void testAbheben() throws GesperrtException {
-		Kunde kunde1 = new Kunde("Timo", "Ji", "Storkower Strasse 219", LocalDate.of(1996, 07, 31));
-		Sparbuch sparbuch1 = new Sparbuch(kunde1, 12345678);
-		assertEquals(33.00, sparbuch1.abheben(33.00));
+	void abhebenPositivValueTest() throws GesperrtException {
+		assertTrue(sparbuch1.abheben(22));
+
+	}
+	@Test
+	void abhebenZeroValueTest() throws GesperrtException {
+		assertTrue(sparbuch1.abheben(0));
+	}
+	@Test
+	void abhebenNegativValueTest() {
+		
 	}
 
 }
